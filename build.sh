436f5046b22e3c78e2d20512c71534e525449308
yafet <yafetmichael36@gmail.com>
2025-06-25 08:55:58 +0300

    docking 2

--- build.sh
+++ build.sh
@@ -2,11 +2,14 @@
 # Exit on error
 set -o errexit
 
+echo "Starting build.sh script..."
 # Install Python dependencies
-pip install -r requirements.txt
+# pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --no-input
+# python manage.py collectstatic --no-input
 
 # Apply database migrations
-python manage.py migrate
+# python manage.py migrate
+
+echo "build.sh finished."
