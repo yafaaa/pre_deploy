9788c8b2efa211fb1c911ace9834c925e634ff5c
yafet <yafetmichael36@gmail.com>
2025-06-27 10:38:37 +0300

    Fix DEBUG parsing

--- project4/settings.py
+++ project4/settings.py
@@ -39,7 +39,7 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY', 'your-default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG', 'False') == 'True'
+DEBUG = env.bool('DEBUG', default=False)
 
 # Dynamically set ALLOWED_HOSTS for Render.com
 ALLOWED_HOSTS = []
